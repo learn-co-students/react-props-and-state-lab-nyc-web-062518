b5d2d72624b4c69fe0a96c3f363658c911f9edd5
jaredianmills <jaredianmills@gmail.com>
2018-08-29 18:00:09 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -24,16 +24,41 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeFilterType} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
       </div>
     )
   }
+
+  changeFilterType = (event) => {
+    this.setState({...this.state, filters: {...this.state.filters, type: event.target.value}})
+  }
+
+  fetchPets = () => {
+    let searchTerm = this.state.filters.type
+    if (searchTerm === 'all') {
+      fetch('/api/pets').then(resp => resp.json()).then(data => this.setState({pets: data}))
+    } else {
+      fetch(`/api/pets?type=${searchTerm}`).then(resp => resp.json()).then(data => this.setState({pets: data}))
+    }
+  }
+
+  onAdoptPet = (petId) => {
+    let updatedPets = this.state.pets.map(pet => {
+      if (pet.id === petId) {
+        return {...pet, isAdopted: true}
+      } else {
+        return pet
+      }
+    })
+    this.setState({pets: updatedPets})
+  }
+
 }
 
 export default App
